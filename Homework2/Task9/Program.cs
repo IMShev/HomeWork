412a3d36df4194cad67b90d69838aeb637844018
Irina Shevchenko <imshev78@gmail.com>
2022-04-25 00:03:42 +0300

    Task 9

---
+++ Homework2/Task9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 случайных целых чисел
+//            и выводит их на экран. Диапазон - любое целое число.
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10000,10000); // если написать .Next() - диапазон будет произвольным 
+    if (i==0)
+        Console.Write("Это массив из восьми случайных чисел - [" + array[i] + ", ");
+    else if (i==array.Length-1)
+        Console.WriteLine(array[i] + "]");
+    else 
+        Console.Write(array[i] + ", ");
+}
+

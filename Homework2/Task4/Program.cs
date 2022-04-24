30a7a7c78c09f75a5ba534e2e90b05508c9d75ec
Irina Shevchenko <imshev78@gmail.com>
2022-04-24 18:59:39 +0300

    Task 4

---
+++ Homework2/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+getSquare(n);
+void getSquare(int n)
+{
+    while (count <= n)
+    {
+        int newn = count;
+        count = count * count * count;
+        Console.WriteLine(newn + " -> " + count);
+        count = newn + 1;
+    }
+}
